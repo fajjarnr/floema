6ae0a6d4e85554858def9b7a4f75525b38ccf6ca
fajjarnr <zfajart@gmail.com>
2021-08-22 11:40:48 +0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const IS_DEVELOPMENT = process.env.NODE_ENV === "dev";
+
+const dirApp = path.join(__dirname, "app");
+const dirAssets = path.join(__dirname, "assets");
+const dirStyles = path.join(__dirname, "styles");
+const dirNode = "node_modules";
+
+module.exports = {
+  entry: [path.join(dirApp, "index.js"), path.join(dirStyles, "index.scss")],
+  resolve: {
+    module: [dirApp, dirAssets, dirStyles, dirNode],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEVELOPMENT,
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+    }),
+  ],
+};
